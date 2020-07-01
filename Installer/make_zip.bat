db8eb73c3f6874be5f77815dce1013c0cb1f9e34
Funway Wang <incawang@msn.com>
2020-07-01 11:01:12 +0800

    1. add zip batch script 2. package some swatches

---
+++ Installer/make_zip.bat
@@ -0,0 +1,10 @@
+set z7z="C:\program files\7-zip\7z.exe"
+
+set zipfile="./bin/Banlan_1.20.630.0.zip"
+del %zipfile%
+
+%z7z% a -tzip %zipfile% ../Src/bin/Release/Banlan.exe 
+%z7z% a -tzip %zipfile% ../Src/bin/Release/Banlan.exe.config
+%z7z% a -tzip %zipfile% ../Src/bin/Release/*.dll
+%z7z% a -tzip %zipfile% ../Swatches
+
